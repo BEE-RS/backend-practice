ee63a00c6c7ca0e506fc4bf337980618b602bdbc
BEE-RS <beeguy.gt@pm.me>
2026-03-19 09:02:25 +0100

    Added tests of the server requests useing REST and implemented more server functionality

--- first_project/server.js
+++ first_project/server.js
@@ -4,14 +4,40 @@ const express = require('express');
 const app = express();
 const PORT = 8756;
 
+const data = ["BEE"]
+
+// Middleware
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    console.log("Endpoint was hit.", req.method);
-    res.sendStatus(200);
+    res.send(`
+        <body style="background-color: pink; color: blue;">
+            <h1>DATA:</h1>
+            <p>${JSON.stringify(data)}</p>
+        </body>
+    `);
 });
 
 app.get('/chill', (req, res) => {
     console.log("Chilling", req.method);
     res.send('Hello World');
+})
+
+app.get('/api/data', (req, res) => {
+    console.log("data was send");
+    res.status(599).send(data);
+})
+
+app.post('/api/data', (req, res) => {
+    const new_entry = req.body
+    data.push(new_entry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/data', (req, res) => {
+    const name = data.pop()
+    console.log(`${name} was deleted.`)
+    res.sendStatus(203)
 })
 
 app.listen(PORT, () => 
